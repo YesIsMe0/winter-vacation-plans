5e9a23f49152e0bd0cee1cf1dad3f77ebc2f0ef5
YesIsMe0 <yefun2004@gmail.com>
2024-01-28 01:21:38 +0800

    修改为凌晨3点前结算

--- src/DataProcessingClass.py
+++ src/DataProcessingClass.py
@@ -2,16 +2,20 @@
 import pandas as pd
 import os
 from datetime import timedelta
+import shutil
+
+Max_study_course = 7  # 最多学习科目数
+
 class DataProcessingClass:
     def __init__(self, current_date):
 
         self.today = current_date  # 初始日期
-        self.course = self.new_today_csv()
-        self.today_course = self.get_course()
+        self.course = self.new_today_csv(current_date)
+        self.today_course = self.get_course(current_date)
 
     # 获取今天的学习任务
-    def get_course(self):
-        today = self.today
+    def get_course(self,current_date):
+        today = current_date
         self.course = pd.read_csv(f"data/{today}.csv", usecols=["学科", "完成情况"])
         self.today_course = self.course[self.course["完成情况"].isnull()][
             "学科"
@@ -25,9 +29,9 @@ class DataProcessingClass:
         return self.course
 
     # 新建今天的csv文件
-    def new_today_csv(self):
+    def new_today_csv(self,current_date):
         # 获取今天的日期
-        today = self.today
+        today = current_date
         yesterday = today - timedelta(days=1)
         # 如果没有昨天的csv文件，就新建一个，有的话就读取昨天的csv文件
         if not os.path.exists(f"data/{yesterday}.csv"):
@@ -37,11 +41,11 @@ class DataProcessingClass:
             self.yesterday_data_process(today)
 
     # 从plan.csv文件新建今天的csv文件
-    def new_today_csv_from_plan(self, today):
+    def new_today_csv_from_plan(self, current_date):
+        today = current_date  # 初始日期
         data = pd.read_csv("data/plan.csv", usecols=["学科", "权值"])
         # 将每一行转换为一个对象
         data_dict = data.to_dict("records")
-
         # 对权值进行降序排序
         data = data.sort_values("权值", ascending=False).reset_index(drop=True)
         # 从高到低依次添加完成情况，初始化为""，第七个开始为0
@@ -55,14 +59,15 @@ class DataProcessingClass:
         return data_dict
 
     # 处理昨天的csv文件
-    def yesterday_data_process(self, today):
+    def yesterday_data_process(self, current_date):
+        today = current_date  # 初始日期
         yesterday = today - timedelta(days=1)
         # 读取昨天的数据
         data_yesterday = pd.read_csv(f"data/{yesterday}.csv")
         # 对每一行进行处理
         for index, row in data_yesterday.iterrows():
             # 如果昨天未完成，调高权重
-            if pd.isnull(row["完成情况"]):
+            if row["完成情况"] == 1:
                 data_yesterday.at[index, "权值"] = self.yesterday_unfinished_weight(
                     row["权值"]
                 )  # +0.7
@@ -72,7 +77,7 @@ class DataProcessingClass:
                     row["权值"]
                 )  # +0.5
             # 如果昨天没有学习，调高权重
-            elif row["完成情况"] == 0:
+            elif row["学习时间"] == 0:
                 data_yesterday.at[index, "权值"] = self.yesterday_no_study_weight(
                     row["权值"]
                 )  # +1
@@ -104,10 +109,10 @@ class DataProcessingClass:
 
         # 从高到低依次添加完成情况，初始化为""，第七个开始为0
         data["完成情况"] = ""
-        data.loc[data.index >= 6, "完成情况"] = 0
+        data.loc[data.index >= Max_study_course, "完成情况"] = 0
         # 添加学习时间列
         data["学习时间"] = ""
-        data.loc[data.index >= 6, "学习时间"] = 0
+        data.loc[data.index >= Max_study_course, "学习时间"] = 0
         # 所有数据保留一位小数
         data = data.round(1)
         # 保存处理后的数据
@@ -141,29 +146,34 @@ class DataProcessingClass:
         return yesterday_date - 0.5
 
     # 输入今天的学习时间和学习情况
-    def input_today_study(self):
+    def input_today_study(self, current_date):
         # 如果 time/{today}.csv 文件不存在，就新建一个,有就退出
-        today = self.today
-        if not os.path.exists(f"time/{self.today}.csv"):
-            
-        
+        today = current_date
+        if not os.path.exists(f"time/{today}.csv"):
             data = pd.read_csv(f"data/{today}.csv")
-            os.system("cls")
+            today_course_temp = pd.read_csv(f"temp/{today}.csv", usecols=["学科"])
+            today_course_list = today_course_temp["学科"].tolist()  # 将 "学科" 列转换为列表
+            # os.system("cls")
             for index, row in data.iterrows():
-                if row["学科"] in self.today_course:
+                if row["学科"] in today_course_list:
                     study_time = input(f"请输入{row['学科']}的学习时间（默认为1.5）：")
                     data.at[index, "学习时间"] = study_time if study_time else "1.5"
             print("=============================================")
             for index, row in data.iterrows():
-                if row["学科"] in self.today_course:
+                if row["学科"] in today_course_list:
                     completion_status = input(f"请输入{row['学科']}的完成情况（默认为2）：")
                     data.at[index, "完成情况"] = (
                         completion_status if completion_status else "2"
                     )
             data.to_csv(f"data/{today}.csv", index=False)
             # 完成输入，将temp文件夹中的文件移动到time文件夹中
-            os.system(f"move temp/{today}.csv time/{today}.csv")
+            if not os.path.exists("time"):
+                os.mkdir("time")
+            
+            shutil.move(f"temp/{today}.csv", f"time/{today}.csv")
+           
             print("将今天的学习情况保存到time文件夹中")
+            return
         else:
             print("今天的学习情况已经输入过了")
             return
--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-
 import DataProcessingClass as dpc
 import schedule_todo_list
 import datetime
@@ -7,19 +6,28 @@ import datetime
 current_time = datetime.datetime.now().time()
 # 获取当前的日期
 current_date = datetime.date.today()
-# 创建 DataProcessingClass 实例
-dpc_instance = dpc.DataProcessingClass(current_date=current_date)
+# 如果没有到第二天的凌晨3点，日期修改为昨天
+if current_time < datetime.time(3, 0):
+    current_date = current_date - datetime.timedelta(days=1)
+    
+# 如果没有到第二天的凌晨3点，时间修改为昨天的23点30分
+current_time = current_time.replace(hour=23, minute=30, second=0, microsecond=0)
 
 
 if current_time < datetime.time(22, 0):
+    print(f"{current_date}的学习任务")
+    # 创建 DataProcessingClass 实例
+    dpc_instance = dpc.DataProcessingClass(current_date=current_date)
     # 如果当前时间是晚上10点前
-    dpc_instance.new_today_csv()
-    dpc_instance.course
+    dpc_instance.new_today_csv(current_date)
+    # dpc_instance.course
     schedule_todo_list = schedule_todo_list.schedule_week(date_today=current_date)
 else:
     print("schedule_night:将在23:00输入今天的学习情况")
     # 如果当前时间是晚上10点或以后
-    schedule_todo_list.schedule_night(current_date=current_date,current_time=current_time)
+    schedule_todo_list.schedule_night(
+        current_date=current_date, current_time=current_time
+    )
 
 """放服务器运行，每天晚上11点更新今天的学习任务"""
 # dpc.input_today_study()
